0814f8b1cc21f58856f010d65690e612dc504161
Greatest Dane <DaneGoodman85@gmail.com>
2019-04-02 17:40:32 -0700

    Been a long time, beginning early stages of logic

--- padEyeLug/padEyeLug.js
+++ padEyeLug/padEyeLug.js
@@ -1,7 +1,28 @@
 let dropDown = $('#drop-down option:selected').val()
 
+console.log(dropDown);
 
 $("#drop-down").on("click", ()=> {
     console.log(dropDown);
 })
 
+
+//Begin logic for the PadEye program 
+
+    if (dropDown === "crosby-g2130x85") {
+        $("#inside-pin").val(1.69);
+        $("#eye-diameter").val(2.38);
+        $("#pin-diameter").val(1.15);
+        $("#lug-pin-hole").val(1.28);
+        $().val();
+        $().val();
+        $().val();
+        $().val();
+        $().val();
+        $().val();
+        $().val();
+        $().val();
+        $().val();
+        $().val();
+        $().val();
+    }
